9f33503deb72e8a6061f11260c3526b82b0a1c93
sylviasami2312 <94935105+sylviasami2312@users.noreply.github.com>
2022-05-28 23:59:42 +0200

    second commit

--- src/entities/user.java
+++ src/entities/user.java
@@ -1,65 +1,81 @@
-package classentity;
-
-import java.io.Serializable;
-import java.lang.Integer;
-import java.lang.String;
+package entities;
 
 import javax.ejb.Stateless;
-import javax.persistence.*;
-import javax.ws.rs.ApplicationPath;
-
-/**
- * Entity implementation class for Entity: user
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Stateless
 @Entity
+public class User{
 
-public class user implements Serializable {
-
-	   
 	@Id
-	private Integer id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int id;
+	
 	private String username;
 	private String password;
 	private String full_name;
 	private String role;
+	private Boolean is_loggedin;
 	private static final long serialVersionUID = 1L;
 
-	public user() {
+	public User() {
 		super();
-	}   
-	public Integer getId() {
-		return this.id;
+		this.is_loggedin=false;
+	}
+	
+	public User(String username, String password, String full_name, String role, Boolean is_loggedin) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.full_name = full_name;
+		this.role = role;
+		this.is_loggedin = false;
 	}
 
-	public void setId(Integer id) {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
 		this.id = id;
-	}   
-	public String getUsername() {
-		return this.username;
 	}
-
+	public String getUsername() {
+		return username;
+	}
 	public void setUsername(String username) {
 		this.username = username;
-	} 
-	public String getPassword() {
-		return this.password;
 	}
-
+	public String getPassword() {
+		return password;
+	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getFullName() {
-		return this.full_name;
+	public String getFull_name() {
+		return full_name;
 	}
-	public void setFullName(String full_name) {
+	public void setFull_name(String full_name) {
 		this.full_name = full_name;
 	}
 	public String getRole() {
-		return this.role;
+		return role;
 	}
 	public void setRole(String role) {
 		this.role = role;
 	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public Boolean getIs_loggedin() {
+		return is_loggedin;
+	}
+	public void setIs_loggedin(Boolean is_loggedin) {
+		this.is_loggedin = is_loggedin;
+	}
+	public void login() {
+		this.is_loggedin=true;
+	}
+		
 }
